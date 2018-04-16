575075f30c7e277e3dee6b9b271c1be630c60d67
Fangzhou Wang <fangzhow@usc.edu>
2018-04-16 13:56:47 -0700

    Updates Nonminimality strategy with short cases

--- Nonminimality_strategy.py
+++ Nonminimality_strategy.py
@@ -9,7 +9,15 @@ class NonminimalityStrategy:
         self.netlist = netlist
         if self.netlist.get_transistors_cnt() <= 1:
             return []
+        self.netlist.update_transistor_names()
+
         for i in range(1, self.netlist.get_transistors_cnt()+1):
+            # generate netlists for open cases
             transistor = self.netlist.remove_transistor(f'M{i:04}', False)
             yield self.netlist.get_netlist_string()
             self.netlist.add_transistor(transistor.get_description())
+
+            # generate netlists for short cases
+            origin_gate_name = self.netlist.short_transistor(f'M{i:04}')
+            yield self.netlist.get_netlist_string()
+            self.netlist.unshort_transistor(f'M{i:04}', origin_gate_name)
