4818b8b3c6b2036d31af30561095c0a5beb49b77
Fangzhou Wang <fangzhow@usc.edu>
2018-04-30 20:28:34 -0700

    Adds a comprehensive test to run all test modules

---
+++ test_all.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+test_modules = [
+    'Circuit.CircuitTest',
+    'CellTest',
+    'MultiCellTest',
+    'StructuralHypoCheckerTest',
+    'ULMTest'
+]
+
+suite = unittest.TestSuite()
+
+for t in test_modules:
+    try:
+        # If the module defines a suite() function, call it to get the suite.
+        mod = __import__(t, globals(), locals(), ['suite'])
+        suitefn = getattr(mod, 'suite')
+        suite.addTest(suitefn())
+    except (ImportError, AttributeError):
+        # else, just load all the test cases from the module.
+        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+
+unittest.TextTestRunner().run(suite)
