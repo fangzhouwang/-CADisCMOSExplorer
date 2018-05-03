230cf7cf356132209f6fc0cc228d7ed08ea56f96
Fangzhou Wang <fangzhow@usc.edu>
2018-05-02 22:30:15 -0700

    Adds checking non-mini using bsf or using bsf_weak

--- Eliminate_nonminimal.py
+++ Eliminate_nonminimal.py
@@ -14,14 +14,20 @@ class NonminimalEliminator:
         self.hypo_checker_ = StructuralHypoChecker()
         self.hypo_checker_.set_strategy(NonminimalityStrategy())
 
-    def eliminate_nonminimal_cells(self, start, cnt):
+    def eliminate_nonminimal_cells(self, start, cnt, is_checking_bsf_weak=True):
         query = f'SELECT idCELL, CELL_NETLIST FROM {self.table_} LIMIT {start},{cnt}'
         nonminimal_cell_ids = list()
         runner_idx = start // cnt
         for row in tqdm(self.db_.run_query_get_all_row(query), desc=f'Eliminating nonminimal[{runner_idx:02}]:'):
             self.hypo_checker_.set_netlist(row['CELL_NETLIST'])
             self.hypo_checker_.check()
-            if not self.hypo_checker_.is_all_bsf_weak_diff():
+
+            if is_checking_bsf_weak:
+                is_non_minimal = not self.hypo_checker_.is_all_bsf_weak_diff()
+            else:
+                is_non_minimal = not self.hypo_checker_.is_all_bsf_diff()
+
+            if is_non_minimal:
                 self.db_.delete_nocommit(row['idCELL'], 'idCELL')
                 nonminimal_cell_ids.append(row['idCELL'])
         self.db_.commit()
